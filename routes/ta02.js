0666ee1d79b9689b14d218b8d0bc6302bb9cca39
Alex <vas17010@byui.edu>
2021-01-14 09:57:54 -0700

    team activity 02

--- routes/ta02.js
+++ routes/ta02.js
@@ -3,13 +3,34 @@
 const express = require('express');
 const router = express.Router();
 
+const usernames = [];
+
 router.get('/',(req, res, next) => {
     res.render('pages/ta02', { 
         title: 'Team Activity 02', 
         path: '/ta02', // For pug, EJS 
         activeTA03: true, // For HBS
         contentCSS: true, // For HBS
+        usernames: usernames,
     });
+});
+
+router.post('/addUser', (req, res, next) =>
+{
+    usernames.push(req.body.username);
+    res.redirect('/ta02');
+});
+
+router.post('/deleteUser', (req, res, next) =>
+{
+    for(i = 0 ; i < usernames.length ; i++)
+    {
+        if(usernames[i] == req.body.name)
+        {
+            usernames.splice(i,1);
+        }
+    }
+    res.redirect('/ta02');
 });
 
 module.exports = router;
