8c8eafb4e983db1106c18f9e5470d0f2ef6a53c8
Alex <vas17010@byui.edu>
2021-01-21 09:29:29 -0700

    team activity 03

--- routes/ta03.js
+++ routes/ta03.js
@@ -1,13 +1,21 @@
 //TA03 PLACEHOLDER
+const { response } = require('express');
 const express = require('express');
 const router = express.Router();
 
+const fetch = require('node-fetch');
+const url = "https://byui-cse.github.io/cse341-course/lesson03/items.json";
+
 router.get('/',(req, res, next) => {
-    res.render('pages/ta03', { 
-        title: 'Team Activity 03', 
-        path: '/ta03', // For pug, EJS 
-        activeTA03: true, // For HBS
-        contentCSS: true, // For HBS
+    fetch(url)
+    .then((response) => response.json())
+    .then((jsObject) => {
+        console.log(jsObject);
+        res.render('pages/ta03', {
+            items: jsObject,
+            title: 'Team Activity 03',
+            path: '/ta03'
+        });
     });
 });
 
