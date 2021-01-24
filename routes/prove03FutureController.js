31b8e4891cc5829055ac510a4a4a34ce6e2466b5
Alex <vas17010@byui.edu>
2021-01-23 23:38:56 -0700

    prove03 smartphones

--- routes/prove03FutureController.js
+++ routes/prove03FutureController.js
@@ -29,11 +29,16 @@ router.get('/getLaptops',(req, res, next) => {
 });
 
 router.get('/getSmartphones',(req, res, next) => {
+    fetch(url)
+    .then((response) => response.json())
+    .then((jsObject) => {
+        console.log(jsObject);
     res.render('pages/prove03/prove03Smartphones', { 
         title: 'Prove 03 : Smartphones', 
         path: '/prove03/smartphones', // For pug, EJS 
         activeTA04: true, // For HBS
         contentCSS: true, // For HBS
+    });
     });
 });
 
